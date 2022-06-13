ad7847135c44a4e4546a6b1a8a7877fa8a65559f
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-13 13:35:55 -0300

    feat: tests

---
+++ src/services/test/auth/login.spec.ts
@@ -0,0 +1,15 @@
+import { AuthService } from "../../index"
+
+describe('[login test]', () => {
+  it('Login to return Auth', async () => {
+    const login = await new AuthService(
+      {
+        nick: "picas",
+        email: "rolamoles@teste.com.br",
+        password: "teste"
+      }
+    ).login()
+    console.log(login)
+    expect(login).toHaveProperty('auth')
+  })
+})
