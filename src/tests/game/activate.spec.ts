4bf83694050554f5a1fd4bb6df24ac6a41f6a3ab
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-15 13:44:01 -0300

    chore:ci test

--- src/tests/game/activate.spec.ts
+++ src/tests/game/activate.spec.ts
@@ -1,8 +1,9 @@
 import { prisma } from "../../config"
 import { AppError } from "../../helpers"
+import 'dotenv/config';
 
 describe('[BETS CREATE]', () => {
   it('CREATE BETS WITH SUCCESS', async () => {
-    console.log('Under maintenance')
+    console.log(process.env.DATABASE_URL)
   })
 })
