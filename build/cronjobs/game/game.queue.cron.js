0ebcf876d667bf257964a4d0eab57d49eb8530c9
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-17 14:28:58 -0300

    Revert "fix: lock for deploy"
    This reverts commit cc619ee2514c1f7d70aef951533d9236c49fe637.

--- build/cronjobs/game/game.queue.cron.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=game.queue.cron.js.map
--- src/scripts/prompt.script.ts
+++ src/scripts/prompt.script.ts
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import 'dotenv/config';
 
 export default function () {
+  console.log(fs.readFileSync(`${__dirname}/monke.txt`).toString());
   console.log(`DB IN USE:`, process.env.DATABASE_URL);
   console.log(`ACTUAL ENV IS`, process.env.NODE_ENV);
   console.log('WELCOME', String(process.env.USER).toUpperCase() || process.env.NAME || 'USER')
